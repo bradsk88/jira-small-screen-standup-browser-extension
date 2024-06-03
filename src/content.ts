92affc9167f450cb7fc5aa3fe8b0a1fa11d60b51
Brad <brad.johnson@hobbii.dk>
2024-06-03 16:05:43 +0200

    Up to 3 cards visible at a time
    TODO:
    - Move owner face to buttons
    - Resolve: some cards missing
    - Add map keyed by [owner+status]
    - Indicate cards' status in UI

--- src/content.ts
+++ src/content.ts
@@ -1,12 +1,13 @@
 import {runtime} from "webextension-polyfill";
+import id = chrome.runtime.id;
 
 console.log("content");
 
 const cardsByOwner: {[owner: string]: HTMLElement[]} = {};
 const facesByOwner: {[owner: string]: string} = {};
 let adjusted: HTMLElement[] = [];
-let currentOwner: string | null = null;
-let currentIndex: number | null = null;
+let currentOwner: string | undefined;
+let currentIndex: number | undefined;
 
 let originalAspectRatio = 1;
 let originalHeight = 100;
@@ -18,10 +19,15 @@ function styleCard(card: HTMLElement) {
     return card;
 }
 
-function adjustWidthAndFont(c: HTMLElement, originalAspectRatio: number, originalHeight: number) {
+function adjustWidthAndFont(
+    c: HTMLElement,
+    originalAspectRatio: number,
+    originalHeight: number,
+    maxWidth: number,
+) {
     const zoomedHeight = c.offsetHeight;
     const zoomedWidth = zoomedHeight / originalAspectRatio;
-    c.style.width = zoomedWidth + "px";
+    c.style.width = Math.min(zoomedWidth, maxWidth) + "px";
     if (adjusted.includes(c)) {
         return;
     }
@@ -33,7 +39,17 @@ function registerWithOwner(card: HTMLElement) {
     const ownerLabel = findElement([card], "id", "-avatar-label")[0];
     const ownerName = ownerLabel?.innerText?.split('Assignee: ')[1];
     cardsByOwner[ownerName] = (cardsByOwner[ownerName] || []).concat(card);
-    facesByOwner[ownerName] = ownerLabel.parentElement!.querySelector('img')!.src;
+    const parentElement = ownerLabel.parentElement;
+    if (!parentElement) {
+        console.warn('No parent for', ownerName, ownerLabel);
+        return;
+    }
+    const img = parentElement.querySelector('img');
+    if (!img) {
+        console.warn('No image for', ownerName, ownerLabel, parentElement);
+        return;
+    }
+    facesByOwner[ownerName] = img.src;
     if (currentOwner == null) {
         currentOwner = ownerName;
     }
@@ -44,34 +60,59 @@ function findElement(cards: HTMLElement[], qualifiedName: string, content: strin
     return cards.filter(v => v.getAttribute(qualifiedName)?.includes(content));
 }
 
-function showNextCard(cardSpot: HTMLDivElement, offset = 1) {
-    const oldIndex = currentIndex;
-    if (currentIndex === null || currentIndex === undefined) {
-        currentIndex = 0;
-    } else {
-        currentIndex += offset;
-    }
-    console.log('index from', oldIndex, 'to', currentIndex);
-
+function getNextCard(
+    offset: number,
+    curOwner: string,
+    requireSameOwner: boolean,
+    curIndex?: number,
+): [HTMLElement | undefined, string, number | undefined] {
+    const oldIndex = curIndex || 0;
+    let nextIndex = oldIndex + offset;
     const owners = Object.keys(facesByOwner);
-    const oldOwner = currentOwner;
+
+    const oldOwner = curOwner;
+    const cards = cardsByOwner[curOwner];
+
+    const ownerIndex = owners.indexOf(curOwner);
+    if (oldIndex >= cards.length) {
+        curOwner = owners[(ownerIndex + offset) % owners.length];
+        if (requireSameOwner && oldOwner != curOwner) {
+            return [undefined, curOwner, oldIndex];
+        }
+        console.log("Took card from owner", oldOwner, "Next owner", curOwner);
+    }
+    console.log("Took card from index", oldIndex, "for owner", oldOwner, "Next index", nextIndex);
+    const card = cardsByOwner[curOwner][oldIndex];
+    return [card, curOwner, nextIndex];
+}
+
+function showNextCard(cardSpot: HTMLDivElement, offset = 1) {
+    const owners = Object.keys(facesByOwner);
     if (!currentOwner) {
         currentOwner = owners[0];
     }
 
-    const cards = cardsByOwner[currentOwner];
-    const ownerIndex = owners.indexOf(currentOwner);
-    if (currentIndex >= cards.length) {
-        currentIndex = 0;
-        currentOwner = owners[(ownerIndex + offset) % owners.length];
-        console.log('owner from', oldOwner, 'to', currentOwner);
-    }
-    cardSpot.innerHTML = "";
-    let card = cardsByOwner[currentOwner][currentIndex];
+    let [card1, owner1, idx1] = getNextCard(offset, currentOwner, false, currentIndex);
+    let [card2, owner2, idx2] = getNextCard(offset, owner1, true, idx1);
+    let [card3, owner3, idx3] = getNextCard(offset, owner1, true, idx2);
 
-    styleCard(card);
-    cardSpot.appendChild(card);
-    requestAnimationFrame(() => adjustWidthAndFont(card, originalAspectRatio, originalHeight));
+    currentOwner = owner3 || owner2 || owner1;
+    if (owner1 === owner3) {
+        currentIndex = idx3;
+    } else {
+        currentIndex = 0;
+    }
+
+    const cards = [card1, card2, card3].filter(v => !!v).map(v => v!);
+
+    cardSpot.innerHTML = "";
+    cards.forEach(card => {
+        styleCard(card);
+        cardSpot.appendChild(card);
+        requestAnimationFrame(() => adjustWidthAndFont(
+            card, originalAspectRatio, originalHeight, cardSpot.offsetWidth / cards.length
+        ));
+    });
 }
 
 runtime.onMessage.addListener(async (message) => {
@@ -88,8 +129,6 @@ runtime.onMessage.addListener(async (message) => {
     overlay.style.flexFlow = 'column';
     overlay.style.gap = '8px';
     overlay.style.zIndex = '99999';
-
-    document.body.appendChild(overlay);
 
     let cards: HTMLElement[] = Array.from(document.querySelectorAll(
         "[data-testid=\"software-board.board-container.board.card-container.card-with-icc\"]",
@@ -131,6 +170,10 @@ runtime.onMessage.addListener(async (message) => {
     cardSpot.style.width = '80%';
     cardSpot.style.margin = '0 auto';
     cardSpot.style.background = 'lightgray';
+    cardSpot.style.display = 'flex';
+    cardSpot.style.flexFlow = 'row nowrap';
+
+
     let prevButton = document.createElement('button');
     prevButton.innerText = 'Prev'
     prevButton.style.width = '200px';
@@ -143,8 +186,10 @@ runtime.onMessage.addListener(async (message) => {
     nextButton.addEventListener('click', () => showNextCard(cardSpot));
     ui.appendChild(nextButton);
 
+
     overlay.appendChild(faces);
     overlay.appendChild(ui);
+    document.body.appendChild(overlay);
     return true;
 });
 
